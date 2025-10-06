b60581d6f55cfaed1021da2a783c42c9bc8f6411
33wvitor <whendersonvitoralveroque@gmail.com>
2025-10-06 11:22:13 -0300

    Mudanca na construcao do banco de dados, dupla conexao para garantir a existencia do banco antes de tentar criar as tabelas

--- src/database/models/models.ts
+++ src/database/models/models.ts
@@ -1,20 +1,38 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'PROJETO_CARTAO_FIDELIDADE',
-    password: '212904',
-    port: 5432,
+// constantes para a conexao do banco de dados
+const DB_NAME = 'PROJETO_CARTAO_FIDELIDADE';
+const DB_USER = 'postgres';
+const DB_PASSWORD = `212904`; // senha do PostgreSQL do local host.
+const DB_HOST = 'localhost';
+const DB_PORT = 5432;
+
+// dados da conexao inicial para criar o banco de dados do zero caso o mesmo nao exista
+const adminPool = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: 'postgres',
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    max: 1
 });
 
+// dados conexao final do banco de dados do projeto
+export const appPool = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+});
 
+// estruturas das tabelas contendo as informacoes
 const schemaInitScript = `
     CREATE TABLE IF NOT EXISTS CLIENTE (
         CPF VARCHAR(11) PRIMARY KEY,
         Nome VARCHAR(45) NOT NULL,
         Telefone VARCHAR(11) NOT NULL,
-        Senha VARCHAR(10) NOT NULL,
+        Senha VARCHAR(10) NOT NULL, 
         Entrou_EM TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
@@ -25,24 +43,42 @@ const schemaInitScript = `
         Senha VARCHAR(10) NOT NULL,
         Entreu_EM TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
-`;
-/* Apenas as tabelas de usuarios foram criadas, precisase obter maior clareza das relacoes e das demais tabelas*/
+`; // as senhas foram limitadas a 10 caracteres, mas pode ser mudada conforme nescessidade
 
+// funcao que inicializa o banco de dados
 export async function initializeDatabase() {
-    try {
-        console.log("Tentando conectar ao banco de dados...");
-        await pool.query('SELECT 1'); 
-        console.log("Conexão estabelecida com sucesso.");
+    console.log(`Verificando existencia do Banco de Dados: ${DB_NAME}`);
 
-        console.log("Verificando e criando o schema se necessário...");
-        await pool.query(schemaInitScript);
+    // tenta conectar ao Postgre admin e criar apartir dai o banco de dados
+    try {
+        await adminPool.query(`CREATE DATABASE ${DB_NAME} IF NOT EXISTS`);
+        console.log(`Database "${DB_NAME}" verificado/criado com sucesso.`);
+
+    // captura o erro de criacao caso haja (o alerta de banco ja existente e ignorado) finaliza a conexao e sai do processo.
+    } catch (error: any) {
+        if (error.code !== '42P04') {
+             console.error("ERRO ao criar o database:", error);
+             adminPool.end();
+             process.exit(1);
+        }
+
+        console.log(`Database "${DB_NAME}" ja existe. Prosseguindo...`);
+
+    // finaliza a conexao com o Postgre admin
+    } finally {
+        await adminPool.end();
+    }
+
+    // tenta criar as tabelesa
+    try {
+        console.log("Conectando ao database do projeto e criando schemas...");
+        await appPool.query(schemaInitScript);
         console.log("Schema inicializado com sucesso.");
 
+    // captura erros, finaliza conexao com o BD, e sai do processo.
     } catch (error) {
-        console.error("ERRO FATAL ao inicializar o banco de dados:", error);
+        console.error("ERRO ao inicializar o Schema:", error);
+        await appPool.end();
         process.exit(1);
-        /* O sistema encerra aqui, futuramente deve se tratar melhor o erro de inicializacao */ 
     }
 }
-
-export default pool;
