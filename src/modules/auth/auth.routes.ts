1b17194148888751c970d344f38d2cb19116d817
Alisson-Vitor18 <alissonvpb18@gmail.com>
2025-10-08 14:35:52 -0300

    Verificação de tokens válidos

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,10 +3,18 @@
 
 import { Router } from "express";
 import { register, login } from "./auth.controller";
+import { autenticarToken } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
+// Rotas públicas
 router.post("/register", register);
 router.post("/login", login);
+
+// Rota protegida — só acessa quem tiver um token válido
+router.get("/me", autenticarToken, (req, res) => {
+  const user = (req as any).user;
+  res.json({ message: "Token válido!", user });
+});
 
 export default router;
